e0cfbdbae71ac60cdcf3789e133f8d056b849c43
hani kumar <132041059+hanikumar0@users.noreply.github.com>
2026-05-06 23:30:00 +0530

    feat: define user and story database schemas

---
+++ backend/models/Story.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const storySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        trim: true
+    },
+    url: {
+        type: String,
+        required: [true, 'Please add a URL'],
+        unique: true
+    },
+    points: {
+        type: Number,
+        default: 0
+    },
+    author: {
+        type: String,
+        required: [true, 'Please add an author']
+    },
+    postedAt: {
+        type: String,
+        required: [true, 'Please add posted time']
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Story', storySchema);
